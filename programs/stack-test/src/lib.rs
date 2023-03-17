98e62cc6984a6e9c7a1e330ab705a0676bfeeceb
blockchainlover2019 <ananphoon823@gmail.com>
2023-03-17 16:45:11 +0000

    feat: add a fail testcase without box

--- programs/stack-test/src/lib.rs
+++ programs/stack-test/src/lib.rs
@@ -11,11 +11,27 @@ declare_id!("G94eEM6q11o6XKuRV83aTbzmr8B56W5RRiC3vgVciwJB");
 pub mod stack_test {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 
     pub fn test_stack(ctx: Context<TestStack>) -> Result<()> {
+        let cpi_program = ctx.accounts.large_stack_program.to_account_info();
+        let cpi_accounts = UseLargeStack {
+            tester: ctx.accounts.tester.to_account_info().clone(),
+            account0: ctx.accounts.account0.to_account_info().clone(),
+            account1: ctx.accounts.account1.to_account_info().clone(),
+            account2: ctx.accounts.account2.to_account_info().clone(),
+            account3: ctx.accounts.account3.to_account_info().clone(),
+            account4: ctx.accounts.account4.to_account_info().clone(),
+            token_program: ctx.accounts.token_program.to_account_info().clone(),
+        };
+        let cpi_context = CpiContext::new(cpi_program, cpi_accounts);
+        use_large_stack(cpi_context)?;
+        Ok(())
+    }
+
+    pub fn test_stack_no_box(ctx: Context<TestStackNoBox>) -> Result<()> {
         let cpi_program = ctx.accounts.large_stack_program.to_account_info();
         let cpi_accounts = UseLargeStack {
             tester: ctx.accounts.tester.to_account_info().clone(),
@@ -50,6 +66,42 @@ pub struct TestStack<'info> {
     pub account4: Box<Account<'info, TokenAccount>>,
     #[account(mut)]
     pub account5: Box<Account<'info, TokenAccount>>,
+    /// CHECK:
+    #[account(mut)]
+    pub account6: AccountInfo<'info>,
+    /// CHECK:
+    #[account(mut)]
+    pub account7: AccountInfo<'info>,
+    /// CHECK:
+    #[account(mut)]
+    pub account8: AccountInfo<'info>,
+    /// CHECK:
+    #[account(mut)]
+    pub account9: AccountInfo<'info>,
+    /// CHECK:
+    #[account(mut)]
+    pub account10: AccountInfo<'info>,
+
+    pub token_program: Program<'info, Token>,
+    pub large_stack_program: Program<'info, LargeStackApp>,
+}
+
+#[derive(Accounts)]
+pub struct TestStackNoBox<'info> {
+    #[account(mut)]
+    pub tester: Signer<'info>,
+    #[account(mut)]
+    pub account0: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub account1: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub account2: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub account3: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub account4: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub account5: Account<'info, TokenAccount>,
     /// CHECK:
     #[account(mut)]
     pub account6: AccountInfo<'info>,
